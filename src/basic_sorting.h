03cab50df8c223c1c188f8965d835bcf7f4f340e
ReyanAndrea <reyanandre@gmail.com>
2026-05-16 21:39:55 +0700

    fungsi fungsinya

---
+++ src/basic_sorting.h
@@ -0,0 +1,13 @@
+#ifndef BASIC_SORTING_H
+#define BASIC_SORTING_H
+
+// Bubble Sort
+void bubbleSort(int arr[], int size);
+
+// Insertion Sort
+void insertionSort(int arr[], int size);
+
+// Selection Sort
+void selectionSort(int arr[], int size);
+
+#endif
